e8b9029868a49d87b0e7afb2cde8b23e733c267e
takashabe <takashabe.io@gmail.com>
2020-11-17 00:59:22 +0900

    bump

---
+++ src/git/go.mod
@@ -0,0 +1,10 @@
+module github.com/takashabe/actions-exercise/src/git
+
+go 1.15
+
+require (
+	github.com/K0kubun/pp v3.0.1+incompatible
+	github.com/go-git/go-git/v5 v5.2.0
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+)
---
+++ src/git/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/K0kubun/pp"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+)
+
+func main() {
+	repo, err := git.PlainOpen(filepath.Clean("../../"))
+	if err != nil {
+		panic(err)
+	}
+	pp.Println(repo)
+
+	h, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+	_, err = repo.CreateTag("src/git/v0.0.1", h.Hash(), &git.CreateTagOptions{
+		Message: "test",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = repo.Push(&git.PushOptions{
+		RemoteName: "origin",
+		Progress:   os.Stdout,
+		RefSpecs:   []config.RefSpec{config.RefSpec("refs/tags/*:refs/tags/*")},
+		Auth: &http.BasicAuth{
+			Username: "bump",
+			Password: os.Getenv("GITHUB_TOKEN"),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
