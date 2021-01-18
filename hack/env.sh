fe274ac70e442ec99350a4c3e508be331787445e
takashabe <takashabe.io@gmail.com>
2021-01-18 18:22:04 +0900

    fix

--- hack/env.sh
+++ hack/env.sh
@@ -5,3 +5,7 @@ set -euo pipefail
 body="/preview env=dev"
 env=$(echo $body | sed -e 's/\/preview env=\(.*\)/\1/')
 echo $env
+
+apply=$(ls -al)
+out="${apply//$'\n'/\\n}"
+echo "::set-output name=out::$out"
